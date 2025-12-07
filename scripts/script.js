fad613bea5ee3229cfbd6d05d6a231cdd9d72a91
Sachinbhute <sachinbhute420@gmail.com>
2025-12-07 16:11:15 +0530

    Added the type writing effect and padding for the hero section

---
+++ scripts/script.js
@@ -0,0 +1,28 @@
+const roles = ["Sachin", "Full-Stack Developer", "AI Learner", "ML Enthusiast","Django Developer"]
+let current = ""
+let i = 0
+let j = 0
+isDeleting = false
+function type() {
+    current = roles[i];
+    
+    if (!isDeleting) {
+        document.getElementById("name").textContent = current.slice(0, ++j);
+    } else {
+        document.getElementById("name").textContent = current.slice(0, --j);
+    }
+
+    if (j === current.length) {
+        isDeleting = true;
+        setTimeout(type, 1000);
+        return;
+    }
+
+    if (j === 0) {
+        isDeleting = false;
+        i = (i + 1) % roles.length;
+    }
+    setTimeout(type, isDeleting ? 50 : 120);
+}
+
+window.onload = type;
